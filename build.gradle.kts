a361f9bd469327d2f6ef8c13c18f93e2625f1b3b
Dmitriy Nosatenko <dmitriynosa@gmail.com>
2024-11-30 12:50:11 +0000

    Linting is not working as expected from the lab.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
-
     kotlin("jvm") version "1.7.10"
+    // Plugin for Dokka - KDoc generating tool
     id("org.jetbrains.dokka") version "1.6.10"
     jacoco
+    // Plugin for Ktlint
     id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
     application
 }
@@ -16,19 +17,21 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation ("io.github.oshai:kotlin-logging-jvm:7.0.0")
-    implementation ("org.jetbrains.kotlin:kotlin-test:1.9.0")
-    implementation ("org.junit.jupiter:junit-jupiter:5.10.0")
+    implementation("io.github.oshai:kotlin-logging-jvm:4.0.0")
+    implementation("org.jetbrains.kotlin:kotlin-test:1.9.10")
+    implementation("org.junit.jupiter:junit-jupiter:5.10.0")
     implementation("org.slf4j:slf4j-simple:2.1.0-alpha1")
     implementation("com.thoughtworks.xstream:xstream:1.4.18")
     implementation("org.codehaus.jettison:jettison:1.4.1")
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.20")
 }
 
 tasks.test {
     useJUnitPlatform()
     finalizedBy(tasks.jacocoTestReport)
 }
+
 kotlin {
-    jvmToolchain(16)
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(16))
+    }
 }
